46f81779ab5562e3454a0b4a847ebc0930888901
vwelschinger <vwelschinger@gmail.com>
2015-10-22 17:55:38 +0200

    Forms updated

--- global.js
+++ global.js
@@ -1,6 +1,24 @@
-
-
 // Custom JS
 $(document).ready(function(){
-  // Add your code here
+  /* ==========================================================================
+     Simple Form - Contact Form
+     ========================================================================== */
+
+  $('#mailbox').submit(function(){
+    $.ajax({
+      dataType: 'jsonp',
+      url: "https://getsimpleform.com/instructions?api_token=0364f378fcb8dba735c43d065e8865d6",
+      data: $("#mailbox").serialize(),
+      error: function() {
+        $('#mailbox').hide();
+        $('#errormessage').show();
+      }
+    }).done(function() {
+      //callback which can be used to show a thank you message
+      //and reset the form
+      $('#mailbox').hide();
+      $('#greatsuccess').show();
+    });
+    return false; //to stop the form from submitting
+  });
 });
